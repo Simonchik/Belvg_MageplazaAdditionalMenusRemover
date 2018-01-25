54fdeaa57f43be57ad4cc6fef8706a52630d9ac2
Alexandr Simonchik <alexandr.simonchik@gmail.com>
2018-01-25 18:32:30 +0300

    first commit

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Belvg_MageplazaAdditionalMenusRemover',
+    __DIR__
+);
